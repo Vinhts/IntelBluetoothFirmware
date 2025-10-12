729b6ac3c60eda028573f1a00521775698e9a5b4
vinhts <98160638+Vinhts@users.noreply.github.com>
2025-10-12 22:08:30 +0700

    Add files via upload

--- IntelBTPatcher/IntelBTPatcher.hpp
+++ IntelBTPatcher/IntelBTPatcher.hpp
@@ -1,94 +1,47 @@
-//
-//  IntelBTPatcher.h
-//  IntelBTPatcher
-//
-//  Created by zxystd <zxystd@foxmail.com> on 2021/2/8.
-//
+#pragma once
 
-#ifndef IntelBTPatcher_h
-#define IntelBTPatcher_h
+#include <Headers/kern_util.hpp>
+#include <Headers/kern_api.hpp>
 
-#include <Headers/kern_patcher.hpp>
-
-#include <IOKit/usb/IOUSBHostDevice.h>
-
-#define DRV_NAME "ibtp"
-
-class BluetoothDeviceAddress;
-
-typedef struct {
-    void *owner;
-    IOMemoryDescriptor *dataBuffer;
-    IOUSBHostCompletionAction action;
-} AsyncOwnerData;
-
-typedef struct __attribute__((packed))
-{
-    uint16_t    opcode;    /* OCF & OGF */
-    uint8_t     len;
-    uint8_t     data[];
-} HciCommandHdr;
-
-typedef struct __attribute__((packed))
-{
-    uint8_t     evt;
-    uint8_t     len;
+typedef struct __attribute__((packed)) {
+    uint8_t evt;
+    uint8_t len;
 } HciEventHdr;
 
-typedef struct __attribute__((packed)) 
-{
+typedef struct __attribute__((packed)) {
     HciEventHdr evt;
-    uint8_t     numCommands;
-    uint16_t    opcode;
-    uint8_t     data[];
-} HciResponse;
+    uint8_t numCommands;
+    uint16_t opcode;
+    uint8_t data[];
+} HciCommand;
 
-const char *requestDirectionNames[] = {
-    "OUT",
-    "IN"
-};
+typedef struct __attribute__((packed)) {
+    uint8_t evt;
+    uint8_t len;
+    uint8_t data[];
+} HciEvent;
 
-const char *requestTypeNames[] = {
-    "Standard",
-    "Class",
-    "Vendor"
-};
-
-const char *requestRecipientNames[] = {
-    "Device",
-    "Interface",
-    "Endpoint",
-    "Other"
-};
-
-const char* _hexDumpHCIData(uint8_t *buf, size_t len)
-{
-    ssize_t str_len = len * 3 + 1;
-    char *str = (char*)IOMalloc(str_len);
-    if (!str)
-        return nullptr;
-    for (size_t i = 0; i < len; i++)
-    snprintf(str + 3 * i, (len - i) * 3, "%02x ", buf[i]);
-    str[MAX(str_len - 2, 0)] = 0;
-    return str;
-}
+typedef struct AsyncOwnerData {
+    void (*action)(void *, void*, IOReturn, uint32_t);
+    void *owner;
+    IOMemoryDescriptor *dataBuffer;
+} AsyncOwnerData;
 
 class CIntelBTPatcher {
 public:
-    bool init();
-    void free();
-    
-    void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size);
-    static IOReturn newFindQueueRequest(void *that, unsigned short arg1, void *addr, unsigned short arg2, bool arg3, void **hciRequestPtr);
-    
-    static IOReturn newHostDeviceRequest(void *that, IOService *provider, StandardUSB::DeviceRequest &request, void *data, IOMemoryDescriptor *descriptor, unsigned int &length,IOUSBHostCompletion *completion, unsigned int timeout);
+    static bool init();
+    static IOReturn newHostDeviceRequest(void *that, IOService *provider, StandardUSB::DeviceRequest &request, void *data, IOMemoryDescriptor *descriptor, unsigned int &length, IOUSBHostCompletion *completion, unsigned int timeout);
+    static IOReturn newAsyncIO(void *that, IOMemoryDescriptor* dataBuffer, uint32_t dataBufferLength, IOUSBHostCompletion* completion, uint32_t completionTimeoutMs);
+    static int newInitPipe(void *that, StandardUSB::EndpointDescriptor const *descriptor, StandardUSB::SuperSpeedEndpointCompanionDescriptor const *superDescriptor, AppleUSBHostController *controller, IOUSBHostDevice *device, IOUSBHostInterface *interface, unsigned char, unsigned short);
 
-    
-    mach_vm_address_t oldFindQueueRequest {};
-    mach_vm_address_t oldHostDeviceRequest {};
-    
+    // placeholders for original saved symbols (names may differ in upstream)
+    static mach_vm_address_t oldFindQueueRequest;
+    static mach_vm_address_t oldHostDeviceRequest;
+    static mach_vm_address_t oldAsyncIO;
+    static mach_vm_address_t oldInitPipe;
+
 private:
+    static void *_hookPipeInstance;
+    static AsyncOwnerData *_interruptPipeAsyncOwner;
     static bool _randomAddressInit;
 };
-
-#endif /* IntelBTPatcher_h */
