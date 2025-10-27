15b2adfc6175075ba7038ab04d29f77f4ac5f0e7
vinhts <98160638+Vinhts@users.noreply.github.com>
2025-10-27 15:22:11 +0700

    Update IntelBTPatcher.hpp
    chang 26/10/2025

--- IntelBTPatcher/IntelBTPatcher.hpp
+++ IntelBTPatcher/IntelBTPatcher.hpp
@@ -1,61 +1,94 @@
 //
-//  IntelBTPatcher.hpp
+//  IntelBTPatcher.h
 //  IntelBTPatcher
 //
-//  Created by lshbluesky on …
-//  Copyright © 2025 lshbluesky. All rights reserved.
+//  Created by zxystd <zxystd@foxmail.com> on 2021/2/8.
 //
 
-#ifndef IntelBTPatcher_hpp
-#define IntelBTPatcher_hpp
+#ifndef IntelBTPatcher_h
+#define IntelBTPatcher_h
 
 #include <Headers/kern_patcher.hpp>
-#include <Headers/kern_api.hpp>
-#include <IOKit/IOService.h>
-#include <IOKit/usb/IOUSBHostPipe.h>
-#include <IOKit/usb/StandardUSB.h>
+
+#include <IOKit/usb/IOUSBHostDevice.h>
+
+#define DRV_NAME "ibtp"
+
+class BluetoothDeviceAddress;
+
+typedef struct {
+    void *owner;
+    IOMemoryDescriptor *dataBuffer;
+    IOUSBHostCompletionAction action;
+} AsyncOwnerData;
+
+typedef struct __attribute__((packed))
+{
+    uint16_t    opcode;    /* OCF & OGF */
+    uint8_t     len;
+    uint8_t     data[];
+} HciCommandHdr;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t     evt;
+    uint8_t     len;
+} HciEventHdr;
+
+typedef struct __attribute__((packed)) 
+{
+    HciEventHdr evt;
+    uint8_t     numCommands;
+    uint16_t    opcode;
+    uint8_t     data[];
+} HciResponse;
+
+const char *requestDirectionNames[] = {
+    "OUT",
+    "IN"
+};
+
+const char *requestTypeNames[] = {
+    "Standard",
+    "Class",
+    "Vendor"
+};
+
+const char *requestRecipientNames[] = {
+    "Device",
+    "Interface",
+    "Endpoint",
+    "Other"
+};
+
+const char* _hexDumpHCIData(uint8_t *buf, size_t len)
+{
+    ssize_t str_len = len * 3 + 1;
+    char *str = (char*)IOMalloc(str_len);
+    if (!str)
+        return nullptr;
+    for (size_t i = 0; i < len; i++)
+    snprintf(str + 3 * i, (len - i) * 3, "%02x ", buf[i]);
+    str[MAX(str_len - 2, 0)] = 0;
+    return str;
+}
 
 class CIntelBTPatcher {
 public:
     bool init();
-    void deinit();
-
-private:
-    static CIntelBTPatcher *callbackIBTPatcher;
-
+    void free();
+    
     void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size);
+    static IOReturn newFindQueueRequest(void *that, unsigned short arg1, void *addr, unsigned short arg2, bool arg3, void **hciRequestPtr);
+    
+    static IOReturn newHostDeviceRequest(void *that, IOService *provider, StandardUSB::DeviceRequest &request, void *data, IOMemoryDescriptor *descriptor, unsigned int &length,IOUSBHostCompletion *completion, unsigned int timeout);
 
-    // ---------- ORIGINAL ROUTES ----------
+    
+    mach_vm_address_t oldFindQueueRequest {};
     mach_vm_address_t oldHostDeviceRequest {};
-    mach_vm_address_t oldAsyncIO {};
-    mach_vm_address_t oldInitPipe {};
-
-    // ---------- FAKE PHY FIX (PR #446) ----------
-    struct AsyncOwnerData {
-        void* owner;
-        IOUSBHostCompletion* action;
-        IOMemoryDescriptor* dataBuffer;
-    };
-
-    static void* _hookPipeInstance;
-    static AsyncOwnerData* _interruptPipeAsyncOwner;
+    
+private:
     static bool _randomAddressInit;
-
-    static IOReturn newAsyncIO(void *that, IOMemoryDescriptor* dataBuffer, uint32_t bytesTransferred,
-                               IOUSBHostCompletion* completion, uint32_t completionTimeoutMs);
-    static int newInitPipe(void *that, StandardUSB::EndpointDescriptor const *descriptor,
-                           StandardUSB::SuperSpeedEndpointCompanionDescriptor const *superDescriptor,
-                           AppleUSBHostController *controller, IOUSBHostDevice *device,
-                           IOUSBHostInterface *interface, unsigned char a7, unsigned short a8);
-    static void asyncIOCompletion(void* owner, void* parameter, IOReturn status, uint32_t bytesTransferred);
-
-    // ---------- ORIGINAL FUNCTIONS ----------
-    static IOReturn wrapHostDeviceRequest(void *that, IOService *provider,
-                                          IOUSBHostIORequest *request,
-                                          IOUSBHostCompletion *completion,
-                                          IOUSBDescriptorHeader *descriptor,
-                                          uint32_t length,
-                                          uint32_t timeout);
 };
 
-#endif /* IntelBTPatcher_hpp */
+#endif /* IntelBTPatcher_h */
