fc53a15fd56f74b24fd11d033de5b1a0ba4d552e
vinhts <98160638+Vinhts@users.noreply.github.com>
2025-10-28 08:33:13 +0700

    Update IntelBTPatcher.cpp
    2

--- IntelBTPatcher/IntelBTPatcher.cpp
+++ IntelBTPatcher/IntelBTPatcher.cpp
@@ -9,7 +9,6 @@
 #include <Headers/kern_util.hpp>
 #include <Headers/plugin_start.hpp>
 #include <IOKit/IOLib.h>
-#include <IOKit/IOMemoryDescriptor.h>
 
 #include "IntelBTPatcher.hpp"
 
@@ -193,165 +192,22 @@ IOReturn CIntelBTPatcher::newFindQueueRequest(void *that, unsigned short arg1, v
 
 #pragma mark - For Monterey+ patch for intercept HCI REQ and RESP
 
-StandardUSB::DeviceRequest randomAddressRequest;
-// Hardcoded Random Address HCI Command
-const uint8_t randomAddressHci[9] = {0x05, 0x20, 0x06, 0x94, 0x50, 0x64, 0xD0, 0x78, 0x6B}; 
-IOBufferMemoryDescriptor *writeHCIDescriptor = nullptr;
-
-#define MAX_HCI_BUF_LEN                 255
-#define HCI_OP_RESET                    0x0c03
-#define HCI_OP_LE_SET_SCAN_PARAM        0x200B
-#define HCI_OP_LE_SET_SCAN_ENABLE       0x200C
-#define HCI_OP_LE_READ_REMOTE_FEATURES  0x2016
-
 IOReturn CIntelBTPatcher::newHostDeviceRequest(void *that, IOService *provider, StandardUSB::DeviceRequest &request, void *data, IOMemoryDescriptor *descriptor, unsigned int &length, IOUSBHostCompletion *completion, unsigned int timeout)
 {
-    HciCommandHdr *hdr = nullptr;
-    uint32_t hdrLen = 0;
-    char hciBuf[MAX_HCI_BUF_LEN] = {0};
-    
-    if (data == nullptr) {
-        if (descriptor != nullptr && descriptor->getLength() > 0) {
-            if (getKernelVersion() < KernelVersion::Sequoia || !descriptor->prepare(kIODirectionOut)) {
-                descriptor->readBytes(0, hciBuf, min(descriptor->getLength(), MAX_HCI_BUF_LEN));
-                hdrLen = (uint32_t)min(descriptor->getLength(), MAX_HCI_BUF_LEN);
-            }
-            if (getKernelVersion() >= KernelVersion::Sequoia)
-                descriptor->complete(kIODirectionOut);
-        }
-        hdr = (HciCommandHdr *)hciBuf;
-        if (hdr->opcode == HCI_OP_LE_SET_SCAN_PARAM) {
-            if (!_randomAddressInit) {
-                randomAddressRequest.bmRequestType = makeDeviceRequestbmRequestType(kRequestDirectionOut, kRequestTypeClass, kRequestRecipientInterface);
-                randomAddressRequest.bRequest = 0xE0;
-                randomAddressRequest.wIndex = 0;
-                randomAddressRequest.wValue = 0;
-                randomAddressRequest.wLength = 9;
-                length = 9;
-                if (writeHCIDescriptor == nullptr)
-                    writeHCIDescriptor = IOBufferMemoryDescriptor::withBytes(randomAddressHci, 9, kIODirectionOut);
-                if (writeHCIDescriptor) {
-                    writeHCIDescriptor->prepare(kIODirectionOut);
-                    IOReturn ret = FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, randomAddressRequest, nullptr, writeHCIDescriptor, length, nullptr, timeout);
-                    writeHCIDescriptor->complete();
-                    const char *randAddressDump = _hexDumpHCIData((uint8_t *)randomAddressHci, 9);
-                    if (randAddressDump) {
-                        SYSLOG(DRV_NAME, "[PATCH] Sending Random Address HCI %d %s", ret, randAddressDump);
-                        IOFree((void *)randAddressDump, 9 * 3 + 1);
-                    }
-                    _randomAddressInit = true;
-                    SYSLOG(DRV_NAME, "[PATCH] Resend LE SCAN PARAM HCI %d", ret);
-                }
-            }
-        } else if (hdr->opcode == HCI_OP_LE_READ_REMOTE_FEATURES && _enablePR446) {
-            // Chỉ xử lý lệnh LE Read Remote Features nếu PR #446 được bật
-            IOReturn ret = FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, request, nullptr, descriptor, length, nullptr, timeout);
-            SYSLOG(DRV_NAME, "[PATCH] Sending extra LE Read Remote Features command %d", ret);
-        }
-    } else {
-        hdr = (HciCommandHdr *)data;
-        hdrLen = request.wLength - 3;
-    }
-    if (hdr) {
-        // HCI reset, we need to send Random address again
-        if (hdr->opcode == HCI_OP_RESET)
-            _randomAddressInit = false;
-#if DEBUG
-        DBGLOG(DRV_NAME, "[%s] bRequest: 0x%x direction: %s type: %s recipient: %s wValue: 0x%02x wIndex: 0x%02x opcode: 0x%04x len: %d length: %d async: %d", provider->getName(), request.bRequest, requestDirectionNames[(request.bmRequestType & kDeviceRequestDirectionMask) >> kDeviceRequestDirectionPhase], requestRecipientNames[(request.bmRequestType & kDeviceRequestRecipientMask) >> kDeviceRequestRecipientPhase], requestTypeNames[(request.bmRequestType & kDeviceRequestTypeMask) >> kDeviceRequestTypePhase], request.wValue, request.wIndex, hdr->opcode, hdr->len, request.wLength, completion != nullptr);
-        if (hdrLen) {
-            const char *dump = _hexDumpHCIData((uint8_t *)hdr, hdrLen);
-            if (dump) {
-                DBGLOG(DRV_NAME, "[Request]: %s", dump);
-                IOFree((void *)dump, hdrLen * 3 + 1);
-            }
-        }
-#endif
-    }
+    // Tạm thời bỏ qua logic phức tạp, chỉ gọi hàm gốc
     return FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, request, data, descriptor, length, completion, timeout);
-}
-
-// Các hàm mới từ PR #446 - chỉ được sử dụng nếu _enablePR446 = true
-#define HCI_EVT_LE_META                               0x3E
-#define HCI_EVT_LE_META_READ_REMOTE_FEATURES_COMPLETE 0x04
-
-uint8_t fakePhyUpdateCompleteEvent[8] = {0x3E, 0x06, 0x0C, 0x00, 0x00, 0x00, 0x02, 0x02};
-
-static void asyncIOCompletion(void* owner, void* parameter, IOReturn status, uint32_t bytesTransferred)
-{
-    // Chỉ xử lý nếu PR #446 được bật
-    if (!callbackIBTPatcher || !callbackIBTPatcher->isPR446Enabled()) {
-        if (owner) {
-            AsyncOwnerData *asyncOwner = (AsyncOwnerData *)owner;
-            if (asyncOwner->action)
-                asyncOwner->action(asyncOwner->owner, parameter, status, bytesTransferred);
-        }
-        return;
-    }
-
-    AsyncOwnerData *asyncOwner = (AsyncOwnerData *)owner;
-    IOMemoryDescriptor* dataBuffer = asyncOwner->dataBuffer;
-    static bool skipExtraReadRemoteFeaturesComplete = true;
-
-    if (dataBuffer && bytesTransferred) {
-        void *buffer = IOMalloc(bytesTransferred);
-        if (buffer) {
-            dataBuffer->readBytes(0, buffer, bytesTransferred);
-            HciEventHdr *hdr = (HciEventHdr *)buffer;
-            if (hdr->evt == HCI_EVT_LE_META && hdr->data[0] == HCI_EVT_LE_META_READ_REMOTE_FEATURES_COMPLETE) {
-                if (skipExtraReadRemoteFeaturesComplete) {
-                    skipExtraReadRemoteFeaturesComplete = false;
-                } else {
-                    // Copy Connection Handle
-                    fakePhyUpdateCompleteEvent[4] = hdr->data[2];
-                    fakePhyUpdateCompleteEvent[5] = hdr->data[3];
-                    dataBuffer->writeBytes(0, fakePhyUpdateCompleteEvent, 8);
-                    skipExtraReadRemoteFeaturesComplete = true;
-                }
-            }
-            IOFree(buffer, bytesTransferred);
-        }
-    }
-    if (asyncOwner->action)
-        asyncOwner->action(asyncOwner->owner, parameter, status, bytesTransferred);
 }
 
 IOReturn CIntelBTPatcher::newAsyncIO(void *that, IOMemoryDescriptor* dataBuffer, uint32_t bytesTransferred, IOUSBHostCompletion* completion, unsigned int completionTimeoutMs)
 {
-    if (_enablePR446 && that == _hookPipeInstance && completion) {
-        if (!_interruptPipeAsyncOwner) {
-            _interruptPipeAsyncOwner = new AsyncOwnerData;
-        }
-        if (_interruptPipeAsyncOwner) {
-            _interruptPipeAsyncOwner->action = completion->action;
-            _interruptPipeAsyncOwner->owner = completion->owner;
-            _interruptPipeAsyncOwner->dataBuffer = dataBuffer;
-            completion->action = asyncIOCompletion;
-            completion->owner = _interruptPipeAsyncOwner;
-        }
-    }
+    // Tạm thời chỉ gọi hàm gốc
     return FunctionCast(newAsyncIO, callbackIBTPatcher->oldAsyncIO)(that, dataBuffer, bytesTransferred, completion, completionTimeoutMs);
 }
 
-#define VENDOR_USB_INTEL 0x8087
-
 int CIntelBTPatcher::newInitPipe(void *that, StandardUSB::EndpointDescriptor const *descriptor, StandardUSB::SuperSpeedEndpointCompanionDescriptor const *superDescriptor, AppleUSBHostController *controller, IOUSBHostDevice *device, IOUSBHostInterface *interface, unsigned char a7, unsigned short a8)
 {
-    int ret = FunctionCast(newInitPipe, callbackIBTPatcher->oldInitPipe)(that, descriptor, superDescriptor, controller, device, interface, a7, a8);
-    if (_enablePR446 && device) {
-        const StandardUSB::DeviceDescriptor *deviceDescriptor = device->getDeviceDescriptor();
-        if (deviceDescriptor &&
-            deviceDescriptor->idVendor == VENDOR_USB_INTEL) {
-            uint8_t epType = StandardUSB::getEndpointType(descriptor);
-            if (epType == kIOUSBEndpointTypeInterrupt) {
-                CIntelBTPatcher::_hookPipeInstance = that;
-                if (!CIntelBTPatcher::_interruptPipeAsyncOwner) {
-                    CIntelBTPatcher::_interruptPipeAsyncOwner = new AsyncOwnerData;
-                }
-                CIntelBTPatcher::_randomAddressInit = false;
-            }
-        }
-    }
-    return ret;
+    // Tạm thời chỉ gọi hàm gốc
+    return FunctionCast(newInitPipe, callbackIBTPatcher->oldInitPipe)(that, descriptor, superDescriptor, controller, device, interface, a7, a8);
 }
 
 bool CIntelBTPatcher::isPR446Enabled() {
